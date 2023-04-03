fe6521c1c8e9638e856f623883da7a9d1ee10910
Nils Söderman <rymdnisse@live.com>
2023-04-03 20:04:43 +0200

    Plugin: Cancel all threads if an exception is thrown

--- pyfbsdk_stub_generator/plugins/plugin.py
+++ pyfbsdk_stub_generator/plugins/plugin.py
@@ -1,7 +1,8 @@
 import threading
+import logging
 import os
 
-from types import ModuleType
+from types import ModuleType, FunctionType
 
 from ..module_types import StubClass, StubFunction, StubParameter, StubProperty
 
@@ -15,6 +16,7 @@ class PluginBaseClass():
         self.ModuleName = Module.__name__
 
         self.bDevMode = os.environ.get("PYFBSDK_DEVMODE") == "True"
+        self.Exceptions = []
 
     def PatchClass(self, Class: StubClass):
         ...
@@ -26,40 +28,39 @@ class PluginBaseClass():
         ...
 
     def PatchEnums(self, ClassList: list[StubClass]):
-        if self.Threading:
-            Threads = []
-            for x in ClassList:
-                Thread = threading.Thread(target=self.PatchEnum, args=(x,))
-                Threads.append(Thread)
-                Thread.start()
-            for Thread in Threads:
-                Thread.join()
-        else:
-            for x in ClassList:
-                self.PatchEnum(x)
+        self._RunPatcher(self.PatchEnum, ClassList)
 
     def PatchClasses(self, ClassList: list[StubClass]):
-        if self.Threading:
-            Threads = []
-            for x in ClassList:
-                Thread = threading.Thread(target=self.PatchClass, args=(x,))
-                Threads.append(Thread)
-                Thread.start()
-            for Thread in Threads:
-                Thread.join()
-        else:
-            for x in ClassList:
-                self.PatchClass(x)
+        self._RunPatcher(self.PatchClass, ClassList)
 
-    def PatchFunctions(self, FunctionList: list[StubFunction]):
+    def PatchFunctions(self, FunctionGroupList: list[list[StubFunction]]):
+        self._RunPatcher(self.PatchFunction, FunctionGroupList)
+
+    def _RunPatcher(self, PatchFunction: FunctionType, StubList: list):
+        StopEvent = threading.Event()
+        
+        def _ThreadedPatcher(self, StubItem):
+            try:
+                PatchFunction(StubItem)
+            except Exception as e:
+                self.Exceptions.append(e)
+                StopEvent.set()
+
         if self.Threading:
             Threads = []
-            for x in FunctionList:
-                Thread = threading.Thread(target=self.PatchFunction, args=(x,))
+            for x in StubList:
+                Thread = threading.Thread(target=_ThreadedPatcher, args=(x,))
                 Threads.append(Thread)
                 Thread.start()
             for Thread in Threads:
                 Thread.join()
+
+            if self.Exceptions:
+                raise self.Exceptions[0]
+            
+            if StopEvent.is_set():
+                for Thread in Threads:
+                    Thread.join()
         else:
-            for x in FunctionList:
-                self.PatchFunction(x)
+            for x in StubList:
+                PatchFunction(x)
