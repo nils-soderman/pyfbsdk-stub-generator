517d24f3a9a5f77ad0d5bf7c20c0bd8f6d653cfd
Nils Söderman <contact@nilssoderman.com>
2023-04-07 10:37:45 +0200

    Removed unused import 'Optional' and disabled pylint in the stub file

--- pyfbsdk_stub_generator/base_content/pyfbsdk.pyi
+++ pyfbsdk_stub_generator/base_content/pyfbsdk.pyi
@@ -2,8 +2,9 @@
 Stub file generated for MotionBuilder {MOTIONBUILDER_VERSION} using:
 https://github.com/nils-soderman/pyfbsdk-stub-generator
 """
+# pylint: disable=all
 from __future__ import annotations
-from typing import overload, Optional, Any
+from typing import overload, Any
 class Enumeration:
     __slots__:tuple
     names:dict
