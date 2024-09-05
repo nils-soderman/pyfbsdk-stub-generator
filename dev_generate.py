59cc1e0ac1d66c98ce46b21a46993c5bba3b7e52
Nils Söderman <rymdnisse@live.com>
2024-09-05 22:16:01 +0200

    Updated requirements & install them before adding them to path

--- dev_generate.py
+++ dev_generate.py
@@ -5,22 +5,23 @@ This script is also used during development to test the generator, therefore it
 reloads all of the modules and caches the online documentation on disk.
 """
 
+import subprocess
 import sys
 import os
 
 from types import ModuleType
 from importlib import reload
 
-import pyfbsdk
-
 CURRENT_DIR = os.path.dirname(__file__)
 REQUIRED_PACKAGES_DIR = os.path.join(CURRENT_DIR, "env")
 
-# Reload all stub generator modules
-for ImportedModule in list(sys.modules.values()):
-    if isinstance(ImportedModule, ModuleType) and "pyfbsdk_stub_generator" in ImportedModule.__name__:
-        reload(ImportedModule)
+# Install required packages
+if not os.path.exists(REQUIRED_PACKAGES_DIR):
+    MoBuPyExe = os.path.join(os.path.dirname(sys.executable), "mobupy.exe")
+    Requirements = os.path.join(CURRENT_DIR, "requirements.txt")
+    subprocess.run([MoBuPyExe, "-m", "pip", "install", "-r", Requirements, "--target", REQUIRED_PACKAGES_DIR], check=True)
 
+# Add the required packages to the path
 for Path in (
     CURRENT_DIR,
     REQUIRED_PACKAGES_DIR,
@@ -29,18 +30,13 @@ for Path in (
         sys.path.append(Path)
 
 
-try:
-    import pyfbsdk_stub_generator
-    from pyfbsdk_stub_generator.stub_generator import GetMotionBuilderVersion
-except ModuleNotFoundError:
-    # Install the required packages
-    import subprocess
-    MoBuPyExe = os.path.join(os.path.dirname(sys.executable), "mobupy.exe")
-    Requirements = os.path.join(CURRENT_DIR, "requirements.txt")
-    subprocess.run([MoBuPyExe, "-m", "pip", "install", "-r", Requirements, "--target", REQUIRED_PACKAGES_DIR], check=True)
+import pyfbsdk_stub_generator
+from pyfbsdk_stub_generator.stub_generator import GetMotionBuilderVersion
 
-    pyfbsdk.FBMessageBox("Restart MotionBuilder", "Please restart MotionBuilder to reload the modules.", "Restart")
-    exit()
+# Reload all stub generator modules
+for ImportedModule in list(sys.modules.values()):
+    if isinstance(ImportedModule, ModuleType) and "pyfbsdk_stub_generator" in ImportedModule.__name__:
+        reload(ImportedModule)
 
 # This will cache the online documentation
 os.environ["PYFBSDK_DEVMODE"] = "True"
